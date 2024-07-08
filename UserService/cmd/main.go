dd5b0bfb9bebc88f10fbd54b2bc34570074045ab
Azizbek <146860466+pronium@users.noreply.github.com>
2024-07-08 11:56:43 +0500

    Add files via upload

---
+++ UserService/cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"EcoTrack/UserServis/config"
+	p "EcoTrack/UserServis/genproto/protos"
+	"EcoTrack/UserServis/pkg/db"
+	service "EcoTrack/UserServis/service"
+	
+	storage "EcoTrack/UserServis/storage/postgres"
+
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	config := config.Load()
+	db, err := db.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer db.Close()
+	listener, err := net.Listen("tcp", config.URL_PORT )
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	defer listener.Close()
+	log.Printf("Server started on port "+config.URL_PORT)
+
+	authStorage := storage.NewAuthRepo(db)
+	userStorage := storage.NewUserRepo(db)
+	us := service.NewUserService(userStorage)
+	as := service.NewAuthService(authStorage)
+
+
+	s := grpc.NewServer()
+	p.RegisterUserServiceServer(s, us)
+	p.RegisterAuthServiceServer(s, as)
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
